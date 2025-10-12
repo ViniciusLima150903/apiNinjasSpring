d6a2cb035460d0797d08f8c6006ff3a607575206
Vinícius Guilherme Pereira de Lima <vdelima965@gmail.com>
2025-10-11 21:37:47 -0300

    first commit

---
+++ CadastroDeNinjasAula/src/main/java/com/vinicius/CadastroDeNinjasAula/CadastroDeNinjasAulaApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.CadastroDeNinjasAula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasAulaApplication.class, args);
+	}
+
+}
