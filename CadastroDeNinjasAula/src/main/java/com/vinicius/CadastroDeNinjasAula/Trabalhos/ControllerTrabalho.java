e95714f5fa0b1f813859e2948364975bf4d34edf
Vinícius Guilherme Pereira de Lima <vdelima965@gmail.com>
2025-10-12 21:27:59 -0300

    Adicionado annoatations na classe de controller de trabalhos

--- CadastroDeNinjasAula/src/main/java/com/vinicius/CadastroDeNinjasAula/Trabalhos/ControllerTrabalho.java
+++ CadastroDeNinjasAula/src/main/java/com/vinicius/CadastroDeNinjasAula/Trabalhos/ControllerTrabalho.java
@@ -1,7 +1,11 @@
 package com.vinicius.CadastroDeNinjasAula.Trabalhos;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping
 public class ControllerTrabalho {
 
     @GetMapping("/trabalhos/welcome")
